0668df9fa5aeafb91583e93db3a6fab996aac67f
Swaminarayan Sharma <120648671+swamisharma@users.noreply.github.com>
2023-03-20 15:50:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,24 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
+  let div1 = document.querySelector("#welcome-div");
+  let div2 = document.querySelector("#about-div");
   return (
     <div id="main">
       <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
       </div>
-      <div id="about-div">
+      <div id="about-div" className={"divDisplay"}>
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
       </div>
-      <button id="toggle">Toggle</button>
+      <button id="toggle" onClick = {() => {
+        div1.classList.toggle("divDisplay");
+        div2.classList.toggle("divDisplay");
+      }}>Toggle</button>
     </div>
   );
 };
